e5428ddc3b481ea564879649d52ebf71e2e784dd
Gamliel Urlanda <g.urlanda@gmail.com>
2022-12-02 23:56:03 -0800

    Factor out Circle component

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
-import './rotate.css';
+import React, { ReactNode } from 'react';
+import styled, { keyframes } from 'styled-components';
 
-const Circle: React.FC<{}> = () => {
+const Circle: React.FC<{
+  children?: ReactNode;
+  speedMs: number;
+  magnitude: number;
+  argumentDeg: number;
+}> = ({ speedMs, magnitude, argumentDeg, children }) => {
+  const ringWidth = 8;
+  const direction = -1;
+  const orbit = keyframes`
+  from {
+  transform: rotate(${-argumentDeg * direction}deg) translate(${
+    magnitude - ringWidth / 2
+  }px) rotate(${argumentDeg * direction}deg);
+  }
+  to {
+  transform: rotate(${-(argumentDeg + 360) * direction}deg) translate(${
+    magnitude - ringWidth / 2
+  }px) rotate(${(argumentDeg + 360) * direction}deg);
+  }
+  `;
+
+  const planetRadius = 10;
+  const OrbitingPlanet = styled.div`
+    height: ${planetRadius * 2}px;
+    width: ${planetRadius * 2}px;
+    animation: ${orbit} ${speedMs}ms linear infinite;
+  `;
+
+  const Ring = styled.div`
+    height: ${magnitude * 2}px;
+    width: ${magnitude * 2}px;
+    border-width: ${10}px;
+    top: ${-(magnitude - planetRadius)}px;
+    left: ${-(magnitude - planetRadius)}px;
+  `;
+
   return (
-    <div className="rounded-full h-48 w-48 border-4 border-blue-600 mt-10 mx-auto flex justify-center items-center">
-      <div className="rounded-full h-10 w-10 bg-green-700 orbit1">
-        <div className="rounded-full h-10 w-10 bg-green-700 orbit2"></div>
-      </div>
-    </div>
+    <Ring className="rounded-full border-blue-600 flex justify-center items-center relative">
+      <OrbitingPlanet className="rounded-full bg-yellow-500 absolute">
+        {children}
+      </OrbitingPlanet>
+    </Ring>
   );
 };
 
