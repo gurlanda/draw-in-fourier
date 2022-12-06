7989b49cc7500963b97a6d0db44e16b30736835a
Gamliel Urlanda <g.urlanda@gmail.com>
2022-12-05 17:24:57 -0800

    refactor: Rename Circle component to FourierRing because it's a better description

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import Circle from './components/Circle';
+import FourierRing from './components/FourierRing';
 
 function App() {
   return (
     <div className="bg-gray-800 h-screen p-4">
       <h1 className="text-3xl text-white font-bold underline">Hello world!</h1>
       <div className="flex justify-center items-center h-full">
-        <Circle
+        <FourierRing
           angularVelocityMillisec={7000}
           radiusPx={100}
           initialAngleDeg={270}
         >
-          <Circle
+          <FourierRing
             angularVelocityMillisec={-2000}
             radiusPx={80}
             initialAngleDeg={90}
           >
-            <Circle
+            <FourierRing
               angularVelocityMillisec={3000}
               radiusPx={50}
               initialAngleDeg={100}
-            ></Circle>
-          </Circle>
-        </Circle>
+            ></FourierRing>
+          </FourierRing>
+        </FourierRing>
       </div>
     </div>
   );
