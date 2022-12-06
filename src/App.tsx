bdd768332fcfc17e9501f5277c980d8def242034
Gamliel Urlanda <g.urlanda@gmail.com>
2022-12-05 17:30:09 -0800

    refactor: Add a container div for the FourierRings

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,25 @@ function App() {
     <div className="bg-gray-800 h-screen p-4">
       <h1 className="text-3xl text-white font-bold underline">Hello world!</h1>
       <div className="flex justify-center items-center h-full">
-        <FourierRing
-          angularVelocityMillisec={7000}
-          radiusPx={100}
-          initialAngleDeg={270}
-        >
+        <div className="relative">
           <FourierRing
-            angularVelocityMillisec={-2000}
-            radiusPx={80}
-            initialAngleDeg={90}
+            angularVelocityMillisec={7000}
+            radiusPx={100}
+            initialAngleDeg={270}
           >
             <FourierRing
-              angularVelocityMillisec={3000}
-              radiusPx={50}
-              initialAngleDeg={100}
-            ></FourierRing>
+              angularVelocityMillisec={-2000}
+              radiusPx={80}
+              initialAngleDeg={90}
+            >
+              <FourierRing
+                angularVelocityMillisec={3000}
+                radiusPx={50}
+                initialAngleDeg={100}
+              ></FourierRing>
+            </FourierRing>
           </FourierRing>
-        </FourierRing>
+        </div>
       </div>
     </div>
   );
