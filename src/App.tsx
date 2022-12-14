f0bab4e615e23d81ae1211b715502d40a9310eb6
Gamliel Urlanda <g.urlanda@gmail.com>
2022-12-13 17:49:09 -0800

    feat: Create animation demo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import DrawingCanvas from './components/DrawingCanvas';
 function App() {
   return (
     <div className="bg-gray-800 h-screen p-4">
-      <h1 className="text-3xl text-white font-bold underline">Hello world!</h1>
       <div className="flex justify-center items-center h-full">
         <div className="relative">
           <FourierRing
