0cc5926cee3d28cfdd88b69efacdaeb392831e40
Gamliel Urlanda <g.urlanda@gmail.com>
2022-12-29 12:42:09 -0800

    Refactor fft so that it only accepts signals that have a power-of-two length

--- src/util/fft.ts
+++ src/util/fft.ts
@@ -1,13 +1,37 @@
 import Complex from './Complex';
+import InvalidArgumentError from './InvalidArgumentError';
+
+/*
+  TODO: Implement zero-padding function
+  TODO: Implement interpolation-padding function
+  TODO: Implement fft for flexible input sizes
+*/
 
 /**
- * Performs the complex-valued FFT. The size of the output signal will be the smallest power of two that is greater than max(signal.length, minOutputSize).
+ * Performs the complex-valued FFT. The size of the input signal must be a positive integer power of two (and therefore non-zero).
  * @param signal The input signal.
  * @param minSize The minimum size of the output signal.
  * @returns The output signal.
+ * @throws {InvalidArgumentError} When the input signal is not a positive integer power of two
  */
-const fft = (signal: Complex[], minOutputSize: number = 0): Complex[] => {
+function fft(signal: Complex[]): Complex[] {
   return [];
-};
+}
+
+/**
+ * Returns true if and only if the input number is a positive integer power of two (and non-zero)
+ * @param num The number to be tested
+ * @returns True if and only if the input number is a positive integer power of two (and non-zero)
+ */
+function isPositivePowerOfTwo(num: number): boolean {
+  if (num === 1) {
+    return true;
+  } else if (num - Math.floor(num) !== 0) {
+    // A non-integer can't be an integer power of two
+    return false;
+  } else {
+    return isPositivePowerOfTwo(num / 2);
+  }
+}
 
 export default fft;
