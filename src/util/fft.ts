caa6a152a84ca71ea0e4714a8fa0218fe70b8d56
Gamliel Urlanda <g.urlanda@gmail.com>
2022-12-29 10:52:47 -0800

    Add empty definition of fft

---
+++ src/util/fft.ts
@@ -0,0 +1,11 @@
+import { complex as Complex } from 'ts-complex-numbers';
+
+/**
+ * Performs the complex-valued FFT. The size of the output signal will be the smallest power of two that is greater than max(signal.length, minOutputSize).
+ * @param signal The input signal.
+ * @param minSize The minimum size of the output signal.
+ * @returns The output signal.
+ */
+const fft = (signal: Complex[], minOutputSize: number = 0): Complex[] => {
+  return [];
+};
