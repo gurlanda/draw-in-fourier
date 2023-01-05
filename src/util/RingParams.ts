047a98e5d96c8fe54981a0b6f757487ae6f7c97c
Gamliel Urlanda <g.urlanda@gmail.com>
2023-01-04 19:54:12 -0800

    Refactor: RingParams constructor now throws the more-specific InvalidArgumentError

--- src/util/RingParams.ts
+++ src/util/RingParams.ts
@@ -1,3 +1,5 @@
+import InvalidArgumentError from './InvalidArgumentError';
+
 /**
  * Represents the set of initial values that defines the motion of a FourierRing.
  */
@@ -7,10 +9,11 @@ class RingParams {
   initialAngleDeg: number;
 
   /**
-   *
+   * Construct a new RingParams object.
    * @param angularVelocityMillisec The velocity of rotation in milliseconds. Positive values specify counterclockwise rotation and negative values specify clockwise rotation.
    * @param radiusPx The radius of the circle in pixels. This must be non-negative or an exception will be thrown upon construction.
    * @param initialAngleDeg The starting angle in degrees, measured from the horizontal axis (x-axis)
+   * @throws {InvalidArgumentError} When the given radiusPx is negative.
    */
   constructor(
     angularVelocityMillisec: number,
@@ -21,7 +24,7 @@ class RingParams {
     this.initialAngleDeg = initialAngleDeg;
 
     if (radiusPx < 0) {
-      throw new TypeError(
+      throw new InvalidArgumentError(
         'In constructor RingParams(): Passed-in argument radiusPx must be a non-negative number'
       );
     }
