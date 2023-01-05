f083e465a75ecfa9d1caa9ca4b60f60b4722ebe4
Gamliel Urlanda <g.urlanda@gmail.com>
2023-01-04 19:50:06 -0800

    Add documentation

--- src/util/RingParams.ts
+++ src/util/RingParams.ts
@@ -1,15 +1,17 @@
 /**
- * A set of initial values that defines the motion of a FourierRing.
- *
- * @param angularVelocityMillisec - The velocity of rotation in milliseconds. Positive values specify counterclockwise rotation and negative values specify clockwise rotation.
- * @param radiusPx - The radius of the circle in pixels. This must be non-negative or an exception will be thrown upon construction
- * @param initialAngleDeg - The starting angle in degrees, measured from the horizontal axis (x-axis)
+ * Represents the set of initial values that defines the motion of a FourierRing.
  */
 class RingParams {
   angularVelocityMillisec: number;
   radiusPx: number;
   initialAngleDeg: number;
 
+  /**
+   *
+   * @param angularVelocityMillisec The velocity of rotation in milliseconds. Positive values specify counterclockwise rotation and negative values specify clockwise rotation.
+   * @param radiusPx The radius of the circle in pixels. This must be non-negative or an exception will be thrown upon construction.
+   * @param initialAngleDeg The starting angle in degrees, measured from the horizontal axis (x-axis)
+   */
   constructor(
     angularVelocityMillisec: number,
     radiusPx: number,
@@ -27,6 +29,10 @@ class RingParams {
     this.radiusPx = radiusPx;
   }
 
+  /**
+   * Creates a deep clone of this instance.
+   * @returns A clone of this instance.
+   */
   clone(): RingParams {
     return new RingParams(
       this.angularVelocityMillisec,
